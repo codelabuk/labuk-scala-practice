03415c75b53aac3e06570a785852b0af489dbdc4
codelabuk <afzal232@gmail.com>
2025-07-03 21:48:45 +0530

    Add function example

--- src/main/scala/basic/Basics.scala
+++ src/main/scala/basic/Basics.scala
@@ -18,16 +18,33 @@ object Basics extends App {
 
   val chainedExpression = {
     if (meaningOfLife > 41) 51
-    else if(meaningOfLife > 23) 9
+    else if (meaningOfLife > 23) 9
     else -1
   }
 
   val codeBlock = {
     // definition
     val aLocalValue = 6
+    // last expression represent value of entire block
     aLocalValue + 8
   }
 
   println(codeBlock)
 
+  def myFunction(x: Int, y: String): String = x + " " + y
+
+  println(myFunction(1, "city"))
+
+  def factorial(n: Int): Int = {
+    if (n <= 1) 1
+    else n * factorial(n - 1)
+  }
+
+  println("factorial = " + factorial(5))
+
+  def myUnitFunction(): Unit = {
+    println("inside unit or void")
+  }
+
+  myUnitFunction()
 }
